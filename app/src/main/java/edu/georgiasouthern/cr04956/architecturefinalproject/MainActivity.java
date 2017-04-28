f26f1ceef00a23225e100022649115e26feb764f
sleevy <chirocam@gmail.com>
2017-04-28 19:22:02 -0400

    added beginning parts of game logic to activity

--- app/src/main/java/edu/georgiasouthern/cr04956/architecturefinalproject/MainActivity.java
+++ app/src/main/java/edu/georgiasouthern/cr04956/architecturefinalproject/MainActivity.java
@@ -2,12 +2,47 @@ package edu.georgiasouthern.cr04956.architecturefinalproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import static edu.georgiasouthern.cr04956.architecturefinalproject.R.id.btnRestart;
 
 public class MainActivity extends AppCompatActivity {
+
+    Game game;
+    ImageView[][] images;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        game = new Game();
+
+        Button btnRestart = (Button) findViewById(R.id.btnRestart);
+        btnRestart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                game.shuffleBoard();
+            }
+        });
+
+        images[0][0] = (ImageView) findViewById(R.id.tile00);
+        images[0][1] = (ImageView) findViewById(R.id.tile01);
+        images[0][2] = (ImageView) findViewById(R.id.tile02);
+
+        images[0][0] = (ImageView) findViewById(R.id.tile00);
+        images[0][0] = (ImageView) findViewById(R.id.tile00);
+        images[0][0] = (ImageView) findViewById(R.id.tile00);
+
+        images[0][0] = (ImageView) findViewById(R.id.tile00);
+        images[0][0] = (ImageView) findViewById(R.id.tile00);
+        images[0][0] = (ImageView) findViewById(R.id.tile00);
     }
+
+    public void updateBoardState() {
+
+    }
+
 }
